2abb3e48a7ea94cd46d2014618e8f0ef061827ec
RadionovAlexey <radionov-aleksei@mail.ru>
2021-07-15 21:15:06 +0600

    created main page

---
+++ 4/index.php
@@ -0,0 +1,46 @@
+<?php
+	$text = file_get_contents('templates/text.tpl');
+
+	if (isset($_POST['send'])) {
+		$string = $_POST['string'];
+		$string = strip_tags($string);
+
+		$userString = str_getcsv($string, " ");
+
+		foreach ($userString as $key => $value) {
+			$newValue = '<span>' . $value . '</span>';
+			$replacement[] = $newValue;
+
+			$value = '/\b(' . $value . ')\b/i';
+			$pattern[] = $value;			
+		}
+
+		$text = preg_replace($pattern, $replacement, $text);		
+	}	
+?>
+
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="styles/style.css" rel="stylesheet">
+	<title>Finder</title>
+</head>
+<body>
+	<h1>Найти строку в тексте</h1>
+	<div class="form"> 
+		<form action="index.php" method="post">	
+			<p><label>Ключевая строка:<input type="text" name="string" required></label></p>
+						
+			<input class="find" name="send" type="submit" value="Поиск">			
+			<hr>
+		</form>
+	</div>
+	<div class="text">
+		<?php
+			echo $text;
+		?>
+	</div>
+</body>
+</html>
